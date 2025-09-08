3b4c35d494a6067856d22abb734b9d278b05de74
Kostiantyn Skliar <skljar.k@gmail.com>
2025-09-08 18:05:22 +0200

    step 1

---
+++ db/users.js
@@ -0,0 +1,27 @@
+import { DataTypes } from "sequelize";
+import sequelize from "./sequelize.js";
+
+const Users = sequelize.define("Users", {
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    subscription: {
+        type: DataTypes.ENUM,
+        values: ["starter", "pro", "business"],
+        defaultValue: "starter"
+    },
+    token: {
+        type: DataTypes.STRING,
+        defaultValue: null,
+    }
+})
+
+await Users.sync({alter: true});
+
+export default Users;
